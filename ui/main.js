2c75cb0ab4ce4138a5188300af4d5ce9e917e516
S2 <shashankshowstoper@gmail.com>
2017-09-06 02:16:27 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,17 +1,19 @@
 console.log('Loaded!');
 var img = document.getElementById('changeimg');
-img.onhover = func()
+function bigsize()
 {
-  img.style.height='150px';
-};
-img.onclick = func1()
+    img.style.height='325px';
+}
+img.onhover = bigsize();
+function imagechange()
 {
-    if(document.getElementById('changeimg')=="http://the-art-of-autism.com/wp-content/uploads/2013/08/CraigRoveta_RefractedLight.jpg")
+    if(document.getElementById("changeimg")=="http://the-art-of-autism.com/wp-content/uploads/2013/08/CraigRoveta_RefractedLight.jpg")
     {
-        document.getElementById('changeimg')="https://www.mupa.hu/f/c9/62/thumb_16211_autistic_art_151210_03-6.jpg/560x350_16211_autistic_art_151210_03-6.jpg";
+        document.getElementById("changeimg")="https://www.mupa.hu/f/c9/62/thumb_16211_autistic_art_151210_03-6.jpg/560x350_16211_autistic_art_151210_03-6.jpg";
     }
     else
     {
-        document.getElementById('changeimg')="http://the-art-of-autism.com/wp-content/uploads/2013/08/CraigRoveta_RefractedLight.jpg";
+        document.getElementById("changeimg")="http://the-art-of-autism.com/wp-content/uploads/2013/08/CraigRoveta_RefractedLight.jpg";
     }
-};
+}
+img.onclick = imagechange();
